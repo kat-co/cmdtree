cada838cb25019fb0fc9da8066c629ff7be1a876
Katherine Cox <cox.katherine.e@gmail.com>
2014-11-13 21:59:21 -0600

    README and standards cleanup.
    - godoc should now be happy.
    - README now reflects godoc.

--- cmdtree.go
+++ cmdtree.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+// CommandExecutor is a function which is run when a command is
+// resolved.
 type CommandExecutor func(args string) error
 
 // CommandExtender represents something that can extend a Command.
@@ -22,18 +24,23 @@ type Command interface {
 	// CommandExtender ensures a Command can extend itself.
 	CommandExtender
 
-	// Execute attempts to parse and execute the given input against
-	// this Command and its sub-commands.
+	// Execute parses the input given with the delimiter defined when
+	// constructing the command. It then executes the command or
+	// sub-command found, and returns any errors.
 	Execute(input string) error
 
 	// String returns the command tree in a human readable format.
 	String() string
 }
 
+// Root creates a new command with no top-level commands. This is
+// useful when you have multiple top-level commands.
 func Root(delimiter string) Command {
 	return NewCmd(delimiter, "", nil)
 }
 
+// NewCmd creates a new command with a top-level command filled out.
+// This is useful for when you are going to extend this command.
 func NewCmd(subCmdDelim, trigger string, executor CommandExecutor) Command {
 	return &command{
 		delimiter: subCmdDelim,
@@ -48,12 +55,14 @@ type command struct {
 	executor           CommandExecutor
 }
 
+// SubCmd implements SubCmd on the CommandExtender interface.
 func (tree *command) SubCmd(trigger string, executor CommandExecutor) Command {
 	subCmd := &command{delimiter: tree.delimiter, trigger: trigger, executor: executor}
 	tree.subCmds = append(tree.subCmds, subCmd)
 	return subCmd
 }
 
+// Execute implements Execute on the Command interface.
 func (tree *command) Execute(input string) error {
 
 	cmdStrs := strings.Split(input, tree.delimiter)
@@ -62,13 +71,13 @@ func (tree *command) Execute(input string) error {
 
 		if cmdStrs[0] != tree.trigger {
 			// It all starts here. Need to at least match self.
-			return fmt.Errorf(`Could not match command "%s"`, input)
+			return fmt.Errorf(`could not match command "%s"`, input)
 		} else if len(tree.subCmds) == 0 || len(cmdStrs) == 1 {
 			// No sub-commands, or no requested sub-commands; just
 			// execute self.
 
 			if tree.executor == nil {
-				return fmt.Errorf("Command usage:\n%s", tree)
+				return fmt.Errorf("command usage:\n%s", tree)
 			}
 
 			return tree.executor(strings.Join(cmdStrs[1:], tree.delimiter))
@@ -89,9 +98,10 @@ func (tree *command) Execute(input string) error {
 		return tree.executor(strings.Join(cmdStrs, tree.delimiter))
 	}
 
-	return fmt.Errorf(`could not match sub-commands "%s".`, cmdStrs[0])
+	return fmt.Errorf(`could not match sub-commands "%s"`, cmdStrs[0])
 }
 
+// String implements String on the Command interface.
 func (tree *command) String() string {
 	outputBuff := new(bytes.Buffer)
 	tree.recurseString(outputBuff, 0)
